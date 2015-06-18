f45c43d5594eaab996e6799567d656fe8ca0ffa0
Arcao <arcao@arcao.com>
2015-06-18 11:24:06 +0200

    Mavenized and migrated to latest appengine

---
+++ src/main/java/com/arcao/wherigoservice/datamapper/ResponseDataMapper.java
@@ -0,0 +1,14 @@
+package com.arcao.wherigoservice.datamapper;
+
+import java.io.IOException;
+import java.util.Map;
+
+public interface ResponseDataMapper {
+	void writeErrorResponse(ResponseCode r, Object... responseTextParam) throws IOException;
+	void writeLoginResponse(String session) throws IOException;
+	void writeGetCartridgeResponse(String cartridgeGuid) throws IOException;
+	void writeGetCartridgeDownloadDataResponse(Map<String, String> formData, String session) throws IOException;
+	void writeGetCacheCodeFromGuidResponse(String cacheGuid) throws IOException;
+	
+	void flush() throws IOException;
+}
